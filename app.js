59d18f6bfbc71982dd3955152389858cd9b52bf4
Vishnu-AV <vishnu.av@perfomatix.com>
2022-01-25 23:41:04 +0530

    added json

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 5500;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello, World!\n');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-var greet = require("./greet");
+var greet = require("./old_greet");
 
 greet();
 var fn = "vishnu";
@@ -24,3 +9,9 @@ console.log(fn);
   var fn = "av";
   console.log(fn);
 }());
+
+// import from greet folder
+
+var newGreet = require('./greet');
+newGreet.english();
+newGreet.spanish();
